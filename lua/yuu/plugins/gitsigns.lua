a72e18f15aa61a63ae728fbed67b89b777360b0d
Nitesh000 <ntudu040@gmail.com>
2023-09-04 13:41:17 +0530

    fix: added the gitsigns keymappings

--- lua/yuu/plugins/gitsigns.lua
+++ lua/yuu/plugins/gitsigns.lua
@@ -1,5 +1,13 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		local keymap = vim.keymap -- for custom keymaps
+
+		-- telescope git commands (not on youtube nvim video)
+		keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+		keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+		keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+		keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+	end,
 }
