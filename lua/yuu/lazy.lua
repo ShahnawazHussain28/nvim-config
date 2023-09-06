617b96cdf4a1389339a48e42e8bc8524abf92728
Nitesh000 <ntudu040@gmail.com>
2023-09-06 12:17:04 +0530

    feat: added config for opts but commented it

--- lua/yuu/lazy.lua
+++ lua/yuu/lazy.lua
@@ -12,6 +12,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- local opts = {
+-- 	install = {
+-- 		colorscheme = { "nightfly" },
+-- 	},
+-- 	checker = {
+-- 		enabled = true,
+-- 		notify = false,
+-- 	},
+-- 	change_detection = {
+-- 		notify = false,
+-- 	},
+-- }
 local opts = {}
 
 -- Configuring Lazy Plugins and options.
