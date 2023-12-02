6c2c79e0e98b03ea7a514bd497e27c7e24db3b08
Nitesh000 <ntudu040@gmail.com>
2023-12-02 16:01:27 +0530

    fix: crates.nvim working

--- lua/yuu/plugins/nvim-cmp.lua
+++ lua/yuu/plugins/nvim-cmp.lua
@@ -8,6 +8,7 @@ return {
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code like pictograms
+		"saecki/crates.nvim", -- rust crates
 	},
 	config = function()
 		local cmp = require("cmp")
